550d5545f234fa2d6970d38ab183122bdbe7917e
TZU\User <c2221a5335@gmail.com>
2022-10-13 21:13:43 +0800

    被丟下車

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -3,6 +3,38 @@ import rain from '../../assets/rain.png'
 
 const fetchData = async () => {
 
+  interface IElement{
+    description: string
+    time: {
+      startTime: string
+      endTime: string
+      elementValue: {
+        value: string
+        measures: string
+      }[]
+    }[]
+  }
+
+  interface IData{
+    location: {
+      datasetDescription: string
+      locationName: string
+      dataid: string
+    }
+  }
+
+  interface ILocation{
+    location: {
+      datasetDescription: string
+      locationName: string
+      dataid: string
+  }
+
+  interface IWeatherResult{
+    records: ILocation
+    success: string
+  }
+  
 
   const response = await fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=rdec-key-123-45678-011121314")
   const responseContent = await response.json()
@@ -13,7 +45,7 @@ const fetchData = async () => {
 
 fetchData()
 
-const location = 'Taichasdasdung'
+const location = 'Taipei'
 
 const WeatherCard = () => {
   return (
@@ -34,7 +66,35 @@ const WeatherCard = () => {
       </div>
 
       <div className='Calendar'>
-        <p>calendar</p>
+
+      <div className='DayChip'>
+
+        </div>
+        
+      <div className='DayChip'>
+
+        </div>
+
+      <div className='DayChip'>
+
+        </div>
+
+      <div className='DayChip'>
+
+        </div>
+
+      <div className='DayChip'>
+
+        </div>
+
+      <div className='DayChip'>
+
+        </div>
+
+      <div className='DayChip'>
+
+        </div>
+
       </div>
 
     </div>
